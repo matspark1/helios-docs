c9486412a5367a02e3bf9dc7e6527a9f3c482aac
Matthew Sparkman <122379993+matspark1@users.noreply.github.com>
2025-02-28 23:24:50 -0500

    some document functionality

---
+++ src/routes/docs/[documentId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onMount } from "svelte";
+  export let data;
+
+  onMount(() => {
+    console.log(data);
+  });
+</script>
+
+<p style="color: white;">Document ID: {data.documentId}</p>
